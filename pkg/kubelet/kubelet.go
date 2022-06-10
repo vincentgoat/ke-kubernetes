dbbd87498455785dcd220274418a9b05ef00ba4f
Shelley-BaoYue <baoyue2@huawei.com>
2022-06-10 11:16:53 +0800

    replace podupdate channel with edgedCh && remove podstorage
    Signed-off-by: Shelley-BaoYue <baoyue2@huawei.com>

--- pkg/kubelet/kubelet.go
+++ pkg/kubelet/kubelet.go
@@ -176,7 +176,6 @@ const (
 	nodeLeaseRenewIntervalFraction = 0.25
 )
 
-var EdgedCh chan kubetypes.PodUpdate
 
 var etcHostsPath = getContainerEtcHostsPath()
 
@@ -250,34 +249,6 @@ type DockerOptions struct {
 	DockerEndpoint            string
 	RuntimeRequestTimeout     time.Duration
 	ImagePullProgressDeadline time.Duration
-}
-
-// makePodSourceConfig creates a config.PodConfig from the given
-// KubeletConfiguration or returns an error.
-func makePodSourceConfig(kubeCfg *kubeletconfiginternal.KubeletConfiguration, kubeDeps *Dependencies, nodeName types.NodeName, nodeHasSynced func() bool) (*config.PodConfig, error) {
-	manifestURLHeader := make(http.Header)
-	if len(kubeCfg.StaticPodURLHeader) > 0 {
-		for k, v := range kubeCfg.StaticPodURLHeader {
-			for i := range v {
-				manifestURLHeader.Add(k, v[i])
-			}
-		}
-	}
-
-	// source of all configuration
-	cfg := config.NewPodConfig(config.PodConfigNotificationIncremental, kubeDeps.Recorder)
-
-	// define file config source
-	if kubeCfg.StaticPodPath != "" {
-		klog.InfoS("Adding static pod path", "path", kubeCfg.StaticPodPath)
-		config.NewSourceFile(kubeCfg.StaticPodPath, nodeName, kubeCfg.FileCheckFrequency.Duration, cfg.Channel(kubetypes.FileSource))
-	}
-
-	if kubeDeps.KubeClient != nil {
-		klog.InfoS("Adding apiserver pod source")
-		config.NewSourceApiserver(kubeDeps.KubeClient, nodeName, nodeHasSynced, cfg.Channel(kubetypes.ApiserverSource))
-	}
-	return cfg, nil
 }
 
 // PreInitRuntimeService will init runtime service before RunKubelet.
@@ -386,14 +357,6 @@ func NewMainKubelet(kubeCfg *kubeletconfiginternal.KubeletConfiguration,
 	nodeIndexer := cache.NewIndexer(cache.MetaNamespaceKeyFunc, cache.Indexers{})
 	nodeLister = corelisters.NewNodeLister(nodeIndexer)
 	nodeHasSynced = func() bool { return true }
-
-	if kubeDeps.PodConfig == nil {
-		var err error
-		kubeDeps.PodConfig, err = makePodSourceConfig(kubeCfg, kubeDeps, nodeName, nodeHasSynced)
-		if err != nil {
-			return nil, err
-		}
-	}
 
 	containerGCPolicy := kubecontainer.GCPolicy{
 		MinAge:             minimumGCAge.Duration,
@@ -1943,7 +1906,7 @@ func (kl *Kubelet) syncLoop(updates <-chan kubetypes.PodUpdate, handler SyncHand
 func (kl *Kubelet) syncLoopIteration(configCh <-chan kubetypes.PodUpdate, handler SyncHandler,
 	syncCh <-chan time.Time, housekeepingCh <-chan time.Time, plegCh <-chan *pleg.PodLifecycleEvent) bool {
 	select {
-	case u, open := <-EdgedCh:
+	case u, open := <-configCh:
 		// Update from a config source; dispatch it to the right handler
 		// callback.
 		if !open {
